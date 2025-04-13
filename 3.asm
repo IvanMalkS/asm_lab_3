ea06f3ddccd4abcb392621519a6272adc6bd4cb4
Van1s1mys <77716298+IvanMalkS@users.noreply.github.com>
2025-04-13 16:14:09 +0500

    Create 3.asm

---
+++ 3.asm
@@ -0,0 +1,26 @@
+ORG 0x0015       ; Тестовые значения
+DB 0x01      
+DB 0x02     
+DB 0x03      
+DB 0x04     
+DB 0x05       
+DB 0x06     
+DB 0x07   
+DB 0x08  
+DB 0x09   
+DB 0x10        
+
+
+LXI H, 0x0015	 
+MVI D, 10       
+SUB A      
+
+LOOP:    
+	ADD M        
+	INX H     
+	DCR D  
+JNZ LOOP         ; Если D != 0, повторяем цикл
+
+MOV M, A      
+HLT      
+
