344cc1f8e18a7f8b79badf54fe2fb6a2e8e9de3c
Van1s1mys <77716298+IvanMalkS@users.noreply.github.com>
2025-04-13 16:06:27 +0500

    Update 2.asm

--- 2.asm
+++ 2.asm
@@ -1,29 +1,36 @@
 ORG 0x0015
-DB 0x00, 0x00, 0x00, 0x01
+DB 0x00, 0x00, 0x01, 0x01
+
+; Сброс всех используемых регистров
+XRA A          ; A = 0
+MOV B, A       ; B = 0
+MOV D, A       ; D = 0
+MOV C, A       ; C = 0
 
 LXI H, 0x0015
-MOV B, M         ; B = x1 = 0x00
+MOV B, M       ; B = x1 = 0x01
 
 INX H
-MOV A, M         ; x2 = 0x00
-CMA              ; A = 0xFF
-MOV D, A         ; D = ¬x2
+MOV A, M       ; x2 = 0x01
+CMA            ; A = 0xFE
+MOV D, A       ; D = ¬x2
 
 INX H
-MOV A, M         ; x3 = 0x00
-CMA              ; A = 0xFF
-ANA B            ; A = ¬x3 AND x1 = 0x00
-MOV C, A         ; C = промежуточный результат
+MOV A, M       ; x3 = 0x00
+CMA            ; A = 0xFF
+ANA B          ; A = ¬x3 ∧ x1 = 0x01
+MOV C, A       ; C = 0x01
 
-MOV A, D         ; A = ¬x2
-XRA C            ; A = ¬x2 XOR (¬x3 AND x1) = 0xFF
+MOV A, D       ; A = ¬x2 = 0xFE
+XRA C          ; A = 0xFF
 
 INX H
-XRA M            ; A = промежуточный XOR x4 = 0xFE
+XRA M          ; A = 0xFE
 
-ANI 0x01         ; Оставляем младший бит (0x00)
+; Двойная проверка результата
+ANI 0x01       ; Должно быть 0x00
+MOV A, A       ; Фиктивная операция для проверки флагов
 
-; Записываем результат
 LXI H, 0x0019
-MOV M, A         ; Гарантированно пишем в 0x0019
+MOV M, A       ; Гарантированно 0x00
 HLT
