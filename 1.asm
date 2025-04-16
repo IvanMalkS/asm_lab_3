a26b165aeb0a7f5da2931e9b97406b888dad70e5
Van1s1mys <77716298+IvanMalkS@users.noreply.github.com>
2025-04-16 11:07:01 +0500

    Update 1.asm

--- 1.asm
+++ 1.asm
@@ -1,24 +1,32 @@
-; Y = X4 - (X2 - X3) - X1
+; Адреса:
+; 0x014: X1 - 05
+; 0x015: X2 - 14
+; 0x016: X3 - 0F
+; 0x017: X4 - 14
+; 0x018: y - 0A
 
-ORG 0x0020
-DB 0x05, 0x14, 0x0F, 0x14   ; X1=5, X2=20, X3=15, X4=20
+; y = (X4 - (X2 - X3)) - X1
 
-LXI H, 0x0021     ; HL = адрес X2
-MOV A, M          ; A = X2 = 20
+LXI H, 0x0014     ; HL = адрес X1
+
+MOV E, M          ; E = X1 (из 0x0100)
+INX H             ; HL = адрес X2
+MOV C, M          ; C = X2 (из 0x0101)
 INX H             ; HL = адрес X3
-SUB M             ; A = X2 - X3 = 5
-MOV D, A          ; D = (X2 - X3) = 5
-
+MOV D, M          ; D = X3 (из 0x0102)
 INX H             ; HL = адрес X4
-MOV A, M          ; A = X4 = 20
-SUB D             ; A = X4 - (X2 - X3) = 15
+MOV A, M          ; A = X4 (из 0x0103)
 
-LXI H, 0x0020     ; HL = адрес X1
-SUB M             ; A = 15 - X1 = 10
+MOV B, A          ; Сохраним X4 (в A) временно в B
+MOV A, C          ; A = X2 (из C)
+SUB D             ; A = X2 - X3
+MOV C, A          ; C = X2 - X3 (Сохраним промежуточный результат в C)
+
+MOV A, B          ; A = X4 (Восстановим X4 из B)
+SUB C             ; A = X4 - (X2 - X3)
+
+SUB E             
 
 INX H
-INX H
-INX H
-INX H             ; HL = 0x0024
-MOV M, A          ; [0x0024] = 0x0A (10)
-HLT
+MOV M, A         
+HLT             
