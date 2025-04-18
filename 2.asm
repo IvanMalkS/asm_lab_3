270aaf2547613b086395e3b5eda67b98c020f44e
Van1s1mys <77716298+IvanMalkS@users.noreply.github.com>
2025-04-18 12:25:26 +0500

    Update 2.asm

--- 2.asm
+++ 2.asm
@@ -1,34 +1,25 @@
-ORG 0x0020
-DB 0x00, 0x00, 0x01, 0x01
+LXI H, 0x001A   ; Point HL to the first input byte (x1)
+MOV B, M       ; B = x1
 
-; Сброс всех используемых регистров
-XRA A          ; A = 0
-MOV B, A       ; B = 0
-MOV D, A       ; D = 0
-MOV C, A       ; C = 0
+INX H          ; Point HL to the second input byte (x2)
+MOV A, M       ; A = x2
+CMA            ; A = NOT x2
+MOV D, A       ; D = NOT x2  (Need to save NOT x2 temporarily)
 
-LXI H, 0x0020
-MOV B, M       ; B = x1 = 0x01
+INX H          ; Point HL to the third input byte (x3)
+MOV A, M       ; A = x3
+CMA            ; A = NOT x3
+ANA B          ; A = (NOT x3) AND x1 (Result C from original code is now in A)
 
-INX H
-MOV A, M       ; x2 = 0x01
-CMA            ; A = 0xFE
-MOV D, A       ; D = ¬x2
+XRA D          ; A = A XOR D = ((NOT x3) AND x1) XOR (NOT x2)
+               ; This is equivalent to D XOR C from original due to XOR commutativity
 
-INX H
-MOV A, M       ; x3 = 0x00
-CMA            ; A = 0xFF
-ANA B          ; A = ¬x3 ∧ x1 = 0x01
-MOV C, A       ; C = 0x01
+INX H          ; Point HL to the fourth input byte (x4)
+XRA M          ; A = A XOR x4 = ((NOT x3) AND x1) XOR (NOT x2) XOR x4
 
-MOV A, D       ; A = ¬x2 = 0xFE
-XRA C          ; A = 0xFF
+ANI 0x01       ; Keep only the least significant bit
 
-INX H
-XRA M          ; A = 0xFE
-
-ANI 0x01       ; Оставляем младший бит 0x00
-
-LXI H, 0x0024
-MOV M, A       ; Гарантированно 0x00
-HLT
+; Store the result
+LXI H, 0x001E   ; Point HL to the output location
+MOV M, A       ; Store the final LSB result
+HLT            ; Halt
